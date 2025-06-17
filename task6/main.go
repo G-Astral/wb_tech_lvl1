14b918712672c21d2ed6b31aab9db7be35d6fd00
G-Astral <super.anakonda2001@yandex.ru>
2025-06-17 17:34:02 +0300

    first method done

--- task6/main.go
+++ task6/main.go
@@ -5,24 +5,55 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
+
+func addText(n int) string {
+	return fmt.Sprintf("ЗДЕСЬ РАБОТАЕТ ГОРУТИНА НОМЕР: %d", n)
+}
+
+func worker(jobs chan int, result chan string, done chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case job := <-jobs:
+			result <- addText(job)
+		case <-done:
+			return
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Способы остановки горутин")
 	fmt.Println("=========================")
-	fmt.Println("\nЧерез закрытие канала:")
+	fmt.Println("\nЧерез done-канал :")
 
-	ch1 := make(chan string, 5)
+	job := make(chan int)
+	result := make(chan string)
+	done := make(chan struct{})
+	workerCounter := 5
+	var wg sync.WaitGroup
+
+	for i := 0; i < workerCounter; i++ {
+		wg.Add(1)
+		go worker(job, result, done, &wg)
+	}
 
 	go func() {
-		ch1 <- "ТУТ РАБОТАЕТ ГОРУТИНА"
-		ch1 <- "ТУТ ГОРУТИНА РАБОТАТЬ НЕ ДОЛЖНА"
-		fmt.Println(<-ch1)
-		close(ch1)
+		for i := range result {
+			fmt.Println(i)
+		}
 	}()
 
-	time.Sleep(10 * time.Millisecond)
+	for i := 0; i < 3; i++ {
+		job <- i + 1
+	}
+
+	close(done)
+	wg.Wait()
+	close(result)
+
 	fmt.Println("Способ завершился.")
 	main2()
 }
